bfb1c04a956e1c847712837a393e9a3a7b11550d
vivek1729 <sasuke.vivek@gmail.com>
2017-10-09 11:43:52 -0700

    Initialized Rails Project

---
+++ match_edu_web_app/app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "Welcome to Match.edu! \n Look out for something awesome in this space!"
+  end
+
+end
