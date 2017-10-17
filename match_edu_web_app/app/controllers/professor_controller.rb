b0d75f547b4ff508be4d414480a5d943dd316e86
vivek1729 <sasuke.vivek@gmail.com>
2017-10-16 20:13:10 -0700

    Added form and create function for Professor

--- match_edu_web_app/app/controllers/professor_controller.rb
+++ match_edu_web_app/app/controllers/professor_controller.rb
@@ -1,5 +1,23 @@
 class ProfessorController < ApplicationController
   def new
-  	
+  	@professor = Professor.new
   end
+
+  def create
+  	#puts('Create method called..')
+  	@professor = Professor.new(professor_params)
+  	if @professor.save
+  		render html: "Saved successfully"
+  	else
+  		render html: "Some error"
+  	end	
+  end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def professor_params
+      params.require(:professor).permit(:name, :email, :picture, :phone, :school, :department)
+    end
+
 end
