be957157703733fc77fb71d259d7ac197e873e44
443582555 <yifu.luo@vanderbilt.edu>
2017-11-09 01:58:14 -0800

    login

---
+++ match_edu_web_app/db/migrate/20171108174149_add_researcharea_to_students.rb
@@ -0,0 +1,5 @@
+class AddResearchareaToStudents < ActiveRecord::Migration[5.1]
+  def change
+    add_column :students, :researcharea, :string
+  end
+end
--- match_edu_web_app/db/schema.rb
+++ match_edu_web_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103042857) do
+ActiveRecord::Schema.define(version: 20171108174149) do
 
   create_table "professor_research_areas", force: :cascade do |t|
     t.integer "professor_id"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20171103042857) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "researcharea"
   end
 
 end
