f921ef86e672af88b077d3f9b0aa483fcde92382
443582555 <yifu.luo@vanderbilt.edu>
2017-11-09 13:02:43 -0800

    merge2

--- match_edu_web_app/config/routes.rb
+++ match_edu_web_app/config/routes.rb
@@ -16,5 +16,4 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
->>>>>>> Stashed changes
 end
