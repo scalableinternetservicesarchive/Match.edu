ddefcbe73a399b4fdbc92e9d9f7673287ab6640e
vivek1729 <sasuke.vivek@gmail.com>
2017-11-17 18:15:11 -0800

    Seed data using raw sql queries isntead of active records

--- match_edu_web_app/db/seeds.rb
+++ match_edu_web_app/db/seeds.rb
@@ -5,16 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 Professor.destroy_all
 ResearchArea.destroy_all
 Student.destroy_all
+ProfessorResearchArea.destroy_all
 p "Cleared existing professor profiles"
 p "Cleared existing research areas"
 
 list_research_areas = ["Distributed Systems",
-						"Parallel Computing","Scalable Systems", 
-						"Networking", "Computer Security",
+            "Parallel Computing","Scalable Systems", 
+            "Networking", "Computer Security",
             "Theoretical Computer Science",
             "Cloud Computing", "Machine Learning",
             "Operating Systems", "Artificial Intelligence"
@@ -23,28 +23,40 @@ research_areas = (0..9).to_a.map do |n|
   ResearchArea.create!(area: list_research_areas[n]) 
 end
 
-100.times do |index|
-  professor = Professor.create!(    
-                name: Faker::Name.unique.name,
-                email: Faker::Internet.unique.email,
-                picture: "https://graydon.law/wp-content/themes/graydon/images/gravatar_default_550.jpg",
-                phone: Faker::PhoneNumber.cell_phone,
-                school: Faker::University.name,
-                department: Faker::Job.field)
-  student = Student.create!(name: Faker::Name.unique.name,
-                email: Faker::Internet.unique.email,
-                password: "password",
-                password_confirmation: "password",
-      researcharea:list_research_areas[index%10])
+#Bulk insert professor data
+values = (0..1000).to_a.map {|u| 
+  "('"+Faker::Name.name.gsub(/\W/, ' ')+"','"+
+  Faker::Internet.email+"',"+
+  "'https://graydon.law/wp-content/themes/graydon/images/gravatar_default_550.jpg','"+
+  Faker::PhoneNumber.cell_phone+"','"+
+  Faker::University.name.gsub(/\W/, ' ')+"','"+
+  Faker::Job.field+"','"+
+  Time.now.utc.to_s(:db)+"','"+
+  Time.now.utc.to_s(:db)+"'"+
+  ")"}.join(",")
+
+ActiveRecord::Base.connection.execute("INSERT INTO professors (name, email, picture, phone, school, department, created_at, updated_at) VALUES #{values}")
+
+#Bulk insert student data
+values = (0..1000).to_a.map {|u| 
+  "('"+Faker::Name.name.gsub(/\W/, ' ')+"','"+
+  Faker::Internet.email+"','"+
+  list_research_areas[u%10]+"','"+
+  Time.now.utc.to_s(:db)+"','"+
+  Time.now.utc.to_s(:db)+"'"+
+  ")"}.join(",")
+
+ActiveRecord::Base.connection.execute("INSERT INTO students (name, email, researcharea, created_at, updated_at) VALUES #{values}")
+
+values = (0..1000).to_a.map {|u|
+  "("+u.to_s+","+
+  rand(research_areas.size).to_s+",'"+
+  Time.now.utc.to_s(:db)+"','"+
+  Time.now.utc.to_s(:db)+"'"+
+  ")"}.join(",")
 
 
-  professor.professor_research_areas.create(
-  research_area: research_areas[rand(research_areas.size)]  
-)
-end	
+ActiveRecord::Base.connection.execute("INSERT INTO professor_research_areas (professor_id, research_area_id, created_at, updated_at) VALUES #{values}")
 
-
-
- 
 p "Created #{Professor.count} professor profiles"
-p "Created #{ResearchArea.count} research areas"
+p "Created #{Student.count} student profiles"
