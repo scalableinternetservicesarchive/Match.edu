dc0bac7b3f0e7027f05732136151b1e4e0cc8831
vivek1729 <sasuke.vivek@gmail.com>
2017-11-27 15:30:23 -0800

    Run seed only if data does NOT exist

--- match_edu_web_app/db/seeds.rb
+++ match_edu_web_app/db/seeds.rb
@@ -5,153 +5,170 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Professor.destroy_all
-ResearchArea.destroy_all
-Student.destroy_all
-ProfessorResearchArea.destroy_all
-p "Cleared existing professor profiles"
-p "Cleared existing research areas"
 
-list_research_areas = [
-            "bvoqeikatoswvso",
-            "uuzlxeablrwmgoe",
-            "oqyvnfxxcoarslt",
-            "ihihwyvybjjtsfy",
-            "hskgxzyxwytvrpk",
-            "pqldshwefozzrde",
-            "haqbiixzesffxjb",
-            "psczcxztmacrpni",
-            "bqxbdydorrnwsqf",
-            "dexohuuwcdrjchy",
-            "fssdpbchxepqvxs",
-            "hufonaygvazmjmj",
-            "xfylunpwrescpsu",
-            "oxtspavpujfvlas",
-            "ioecxmjvnjbjbga",
-            "htrlpelluxvyyvp",
-            "kvqeogdkkwgodfi",
-            "somrbcrugtyxehs",
-            "vxrknxjkuzhfxhn",
-            "vllpvtaxklmlukr",
-            "kmmdkwkgwcdqkcu",
-            "qikrlvjxuzfdvah",
-            "kvxbliihsuxbgch",
-            "fsllkseisnnyxmi",
-            "vfhigiweqitlosi",
-            "qbbezqqprrfjlvz",
-            "ofraabafdavphbl",
-            "dubpmyannsanunh",
-            "evwtwksdlawyjmf",
-            "cnhmyerltxptakm",
-            "vjjrfdhqagrbdac",
-            "bnvmprhcmsctfkb",
-            "hadwyenocxfkmzb",
-            "siunbmefcsjcmac",
-            "xrujnacyczzcnio",
-            "yjkpruzspmvxsby",
-            "pupynnirzjmohfd",
-            "dimjftqgtyuotja",
-            "pnwipgxctpfdxwb",
-            "nqelvwfrxtcrfya",
-            "tdbqehwsruwuxbb",
-            "lrcleeeqnnopmsj",
-            "kyjsqxttuldfrby",
-            "fvqdhjkhzzonrdh",
-            "vrsypdzurgzbbvr",
-            "thaflgwjzqwsegk",
-            "lazbkfgqgnumhkm",
-            "efmspulgsictzds",
-            "rfmjgmdtdhvkopr",
-            "qblmmkiadkqyrem",
-            "gvrjkfskipwqbvh",
-            "iqubrlzytieachi",
-            "prlazklvgvwywjl",
-            "xajgddpnlscvvjd",
-            "ijutiwgbtkcoopp",
-            "tomzpzckoobhimr",
-            "heigdkyubvtffxx",
-            "uturordxgsxkcyk",
-            "lniynaxgsdbvtfx",
-            "taypnwqxfkgcfwu",
-            "vfxjzoahqepxpdc",
-            "zzlzbfhkunfptdb",
-            "abmpowkecsqlckf",
-            "ubzkmeqmbczwptj",
-            "jvhypibngmboqiw",
-            "vwernpfcvrytyfh",
-            "hslfvuckjoguogs",
-            "krjbhgfbopcdrhx",
-            "pkfzstpqqzyhegf",
-            "fmadsastetfyngm",
-            "jnbpdlfotbxeiqx",
-            "bbwrzybsdkxjefr",
-            "cwhvgickxdbgoej",
-            "dhujakcyyshukoq",
-            "susrxegeifqojdp",
-            "fvtioalpasnwugo",
-            "xfuesintdjlbeck",
-            "qvuufyqyuqyseqc",
-            "kotmainrkcbyyyc",
-            "dqcfwputznykryg",
-            "zyndgqxhlzrazfu",
-            "tsolhlmxohqslgv",
-            "xrqjnnkeqsinfvo",
-            "vwdtywyityvdkxp",
-            "jfyjhxxhdvnqiyd",
-            "phzvgsszxjjthip",
-            "oerqcuhmdcdmnmm",
-            "bzcmmsqorstvnlr",
-            "bswvynqtzzavtgw",
-            "jmhxskbwexmnanu",
-            "gdzschckizuefwf",
-            "dvyflacdonpbjkt",
-            "zjaviuksjywwfdn",
-            "ceqkirbubfrrsse",
-            "otfmibpgylnjrqu",
-            "rvrxhqqnluyofoo",
-            "hjsyqlloehdxojd",
-            "sukjryaagunivlc",
-            "eqldbslevphgxud",
-            "xxnnlyotdhzvwjy"
-          ]
-research_areas = (0..99).to_a.map do |n|
-  ResearchArea.create!(area: list_research_areas[n]) 
+def populate_data
+
+    Professor.destroy_all
+    ResearchArea.destroy_all
+    Student.destroy_all
+    ProfessorResearchArea.destroy_all
+    p "Cleared existing professor profiles"
+    p "Cleared existing research areas"
+
+    list_research_areas = [
+                "bvoqeikatoswvso",
+                "uuzlxeablrwmgoe",
+                "oqyvnfxxcoarslt",
+                "ihihwyvybjjtsfy",
+                "hskgxzyxwytvrpk",
+                "pqldshwefozzrde",
+                "haqbiixzesffxjb",
+                "psczcxztmacrpni",
+                "bqxbdydorrnwsqf",
+                "dexohuuwcdrjchy",
+                "fssdpbchxepqvxs",
+                "hufonaygvazmjmj",
+                "xfylunpwrescpsu",
+                "oxtspavpujfvlas",
+                "ioecxmjvnjbjbga",
+                "htrlpelluxvyyvp",
+                "kvqeogdkkwgodfi",
+                "somrbcrugtyxehs",
+                "vxrknxjkuzhfxhn",
+                "vllpvtaxklmlukr",
+                "kmmdkwkgwcdqkcu",
+                "qikrlvjxuzfdvah",
+                "kvxbliihsuxbgch",
+                "fsllkseisnnyxmi",
+                "vfhigiweqitlosi",
+                "qbbezqqprrfjlvz",
+                "ofraabafdavphbl",
+                "dubpmyannsanunh",
+                "evwtwksdlawyjmf",
+                "cnhmyerltxptakm",
+                "vjjrfdhqagrbdac",
+                "bnvmprhcmsctfkb",
+                "hadwyenocxfkmzb",
+                "siunbmefcsjcmac",
+                "xrujnacyczzcnio",
+                "yjkpruzspmvxsby",
+                "pupynnirzjmohfd",
+                "dimjftqgtyuotja",
+                "pnwipgxctpfdxwb",
+                "nqelvwfrxtcrfya",
+                "tdbqehwsruwuxbb",
+                "lrcleeeqnnopmsj",
+                "kyjsqxttuldfrby",
+                "fvqdhjkhzzonrdh",
+                "vrsypdzurgzbbvr",
+                "thaflgwjzqwsegk",
+                "lazbkfgqgnumhkm",
+                "efmspulgsictzds",
+                "rfmjgmdtdhvkopr",
+                "qblmmkiadkqyrem",
+                "gvrjkfskipwqbvh",
+                "iqubrlzytieachi",
+                "prlazklvgvwywjl",
+                "xajgddpnlscvvjd",
+                "ijutiwgbtkcoopp",
+                "tomzpzckoobhimr",
+                "heigdkyubvtffxx",
+                "uturordxgsxkcyk",
+                "lniynaxgsdbvtfx",
+                "taypnwqxfkgcfwu",
+                "vfxjzoahqepxpdc",
+                "zzlzbfhkunfptdb",
+                "abmpowkecsqlckf",
+                "ubzkmeqmbczwptj",
+                "jvhypibngmboqiw",
+                "vwernpfcvrytyfh",
+                "hslfvuckjoguogs",
+                "krjbhgfbopcdrhx",
+                "pkfzstpqqzyhegf",
+                "fmadsastetfyngm",
+                "jnbpdlfotbxeiqx",
+                "bbwrzybsdkxjefr",
+                "cwhvgickxdbgoej",
+                "dhujakcyyshukoq",
+                "susrxegeifqojdp",
+                "fvtioalpasnwugo",
+                "xfuesintdjlbeck",
+                "qvuufyqyuqyseqc",
+                "kotmainrkcbyyyc",
+                "dqcfwputznykryg",
+                "zyndgqxhlzrazfu",
+                "tsolhlmxohqslgv",
+                "xrqjnnkeqsinfvo",
+                "vwdtywyityvdkxp",
+                "jfyjhxxhdvnqiyd",
+                "phzvgsszxjjthip",
+                "oerqcuhmdcdmnmm",
+                "bzcmmsqorstvnlr",
+                "bswvynqtzzavtgw",
+                "jmhxskbwexmnanu",
+                "gdzschckizuefwf",
+                "dvyflacdonpbjkt",
+                "zjaviuksjywwfdn",
+                "ceqkirbubfrrsse",
+                "otfmibpgylnjrqu",
+                "rvrxhqqnluyofoo",
+                "hjsyqlloehdxojd",
+                "sukjryaagunivlc",
+                "eqldbslevphgxud",
+                "xxnnlyotdhzvwjy"
+              ]
+    research_areas = (0..99).to_a.map do |n|
+      ResearchArea.create!(area: list_research_areas[n]) 
+    end
+
+    #Bulk insert professor data
+    values = (0..50000).to_a.map {|u| 
+      "('"+Faker::Name.name.gsub(/\W/, ' ')+"','"+
+      Faker::Internet.email+"',"+
+      "'https://graydon.law/wp-content/themes/graydon/images/gravatar_default_550.jpg','"+
+      Faker::PhoneNumber.cell_phone+"','"+
+      Faker::University.name.gsub(/\W/, ' ')+"','"+
+      Faker::Job.field+"','"+
+      Time.now.utc.to_s(:db)+"','"+
+      Time.now.utc.to_s(:db)+"'"+
+      ")"}.join(",")
+
+    ActiveRecord::Base.connection.execute("INSERT INTO professors (name, email, picture, phone, school, department, created_at, updated_at) VALUES #{values}")
+
+    #Bulk insert student data
+    values = (0..50000).to_a.map {|u| 
+      "('"+Faker::Name.name.gsub(/\W/, ' ')+"','"+
+      Faker::Internet.email+"','"+
+      list_research_areas[u%10]+"','"+
+      Time.now.utc.to_s(:db)+"','"+
+      Time.now.utc.to_s(:db)+"'"+
+      ")"}.join(",")
+
+    ActiveRecord::Base.connection.execute("INSERT INTO students (name, email, researcharea, created_at, updated_at) VALUES #{values}")
+
+    values = (0..50000).to_a.map {|u|
+      "("+u.to_s+","+
+      rand(research_areas.size).to_s+",'"+
+      Time.now.utc.to_s(:db)+"','"+
+      Time.now.utc.to_s(:db)+"'"+
+      ")"}.join(",")
+
+
+    ActiveRecord::Base.connection.execute("INSERT INTO professor_research_areas (professor_id, research_area_id, created_at, updated_at) VALUES #{values}")
+
+    p "Created #{Professor.count} professor profiles"
+    p "Created #{Student.count} student profiles"
 end
 
-#Bulk insert professor data
-values = (0..50000).to_a.map {|u| 
-  "('"+Faker::Name.name.gsub(/\W/, ' ')+"','"+
-  Faker::Internet.email+"',"+
-  "'https://graydon.law/wp-content/themes/graydon/images/gravatar_default_550.jpg','"+
-  Faker::PhoneNumber.cell_phone+"','"+
-  Faker::University.name.gsub(/\W/, ' ')+"','"+
-  Faker::Job.field+"','"+
-  Time.now.utc.to_s(:db)+"','"+
-  Time.now.utc.to_s(:db)+"'"+
-  ")"}.join(",")
-
-ActiveRecord::Base.connection.execute("INSERT INTO professors (name, email, picture, phone, school, department, created_at, updated_at) VALUES #{values}")
-
-#Bulk insert student data
-values = (0..50000).to_a.map {|u| 
-  "('"+Faker::Name.name.gsub(/\W/, ' ')+"','"+
-  Faker::Internet.email+"','"+
-  list_research_areas[u%10]+"','"+
-  Time.now.utc.to_s(:db)+"','"+
-  Time.now.utc.to_s(:db)+"'"+
-  ")"}.join(",")
-
-ActiveRecord::Base.connection.execute("INSERT INTO students (name, email, researcharea, created_at, updated_at) VALUES #{values}")
-
-values = (0..50000).to_a.map {|u|
-  "("+u.to_s+","+
-  rand(research_areas.size).to_s+",'"+
-  Time.now.utc.to_s(:db)+"','"+
-  Time.now.utc.to_s(:db)+"'"+
-  ")"}.join(",")
+p "Currently #{Professor.count} professor profiles exist"
+if Professor.count != 50001
+    puts("Populate data")
+    populate_data
+else
+    puts("Data alread exists. No need to re-enter data.")
+end
 
 
-ActiveRecord::Base.connection.execute("INSERT INTO professor_research_areas (professor_id, research_area_id, created_at, updated_at) VALUES #{values}")
 
-p "Created #{Professor.count} professor profiles"
-p "Created #{Student.count} student profiles"
+
+
