686bb787e9060d5d79769c2bd696cd5c3e03d4aa
vivek1729 <sasuke.vivek@gmail.com>
2017-11-27 14:31:52 -0800

    Added 50k users and 100 research areas

--- match_edu_web_app/db/seeds.rb
+++ match_edu_web_app/db/seeds.rb
@@ -12,19 +12,114 @@ ProfessorResearchArea.destroy_all
 p "Cleared existing professor profiles"
 p "Cleared existing research areas"
 
-list_research_areas = ["Distributed Systems",
-            "Parallel Computing","Scalable Systems", 
-            "Networking", "Computer Security",
-            "Theoretical Computer Science",
-            "Cloud Computing", "Machine Learning",
-            "Operating Systems", "Artificial Intelligence"
+list_research_areas = [
+            "bvoqeikatoswvso",
+            "uuzlxeablrwmgoe",
+            "oqyvnfxxcoarslt",
+            "ihihwyvybjjtsfy",
+            "hskgxzyxwytvrpk",
+            "pqldshwefozzrde",
+            "haqbiixzesffxjb",
+            "psczcxztmacrpni",
+            "bqxbdydorrnwsqf",
+            "dexohuuwcdrjchy",
+            "fssdpbchxepqvxs",
+            "hufonaygvazmjmj",
+            "xfylunpwrescpsu",
+            "oxtspavpujfvlas",
+            "ioecxmjvnjbjbga",
+            "htrlpelluxvyyvp",
+            "kvqeogdkkwgodfi",
+            "somrbcrugtyxehs",
+            "vxrknxjkuzhfxhn",
+            "vllpvtaxklmlukr",
+            "kmmdkwkgwcdqkcu",
+            "qikrlvjxuzfdvah",
+            "kvxbliihsuxbgch",
+            "fsllkseisnnyxmi",
+            "vfhigiweqitlosi",
+            "qbbezqqprrfjlvz",
+            "ofraabafdavphbl",
+            "dubpmyannsanunh",
+            "evwtwksdlawyjmf",
+            "cnhmyerltxptakm",
+            "vjjrfdhqagrbdac",
+            "bnvmprhcmsctfkb",
+            "hadwyenocxfkmzb",
+            "siunbmefcsjcmac",
+            "xrujnacyczzcnio",
+            "yjkpruzspmvxsby",
+            "pupynnirzjmohfd",
+            "dimjftqgtyuotja",
+            "pnwipgxctpfdxwb",
+            "nqelvwfrxtcrfya",
+            "tdbqehwsruwuxbb",
+            "lrcleeeqnnopmsj",
+            "kyjsqxttuldfrby",
+            "fvqdhjkhzzonrdh",
+            "vrsypdzurgzbbvr",
+            "thaflgwjzqwsegk",
+            "lazbkfgqgnumhkm",
+            "efmspulgsictzds",
+            "rfmjgmdtdhvkopr",
+            "qblmmkiadkqyrem",
+            "gvrjkfskipwqbvh",
+            "iqubrlzytieachi",
+            "prlazklvgvwywjl",
+            "xajgddpnlscvvjd",
+            "ijutiwgbtkcoopp",
+            "tomzpzckoobhimr",
+            "heigdkyubvtffxx",
+            "uturordxgsxkcyk",
+            "lniynaxgsdbvtfx",
+            "taypnwqxfkgcfwu",
+            "vfxjzoahqepxpdc",
+            "zzlzbfhkunfptdb",
+            "abmpowkecsqlckf",
+            "ubzkmeqmbczwptj",
+            "jvhypibngmboqiw",
+            "vwernpfcvrytyfh",
+            "hslfvuckjoguogs",
+            "krjbhgfbopcdrhx",
+            "pkfzstpqqzyhegf",
+            "fmadsastetfyngm",
+            "jnbpdlfotbxeiqx",
+            "bbwrzybsdkxjefr",
+            "cwhvgickxdbgoej",
+            "dhujakcyyshukoq",
+            "susrxegeifqojdp",
+            "fvtioalpasnwugo",
+            "xfuesintdjlbeck",
+            "qvuufyqyuqyseqc",
+            "kotmainrkcbyyyc",
+            "dqcfwputznykryg",
+            "zyndgqxhlzrazfu",
+            "tsolhlmxohqslgv",
+            "xrqjnnkeqsinfvo",
+            "vwdtywyityvdkxp",
+            "jfyjhxxhdvnqiyd",
+            "phzvgsszxjjthip",
+            "oerqcuhmdcdmnmm",
+            "bzcmmsqorstvnlr",
+            "bswvynqtzzavtgw",
+            "jmhxskbwexmnanu",
+            "gdzschckizuefwf",
+            "dvyflacdonpbjkt",
+            "zjaviuksjywwfdn",
+            "ceqkirbubfrrsse",
+            "otfmibpgylnjrqu",
+            "rvrxhqqnluyofoo",
+            "hjsyqlloehdxojd",
+            "sukjryaagunivlc",
+            "eqldbslevphgxud",
+            "xxnnlyotdhzvwjy"
           ]
-research_areas = (0..9).to_a.map do |n|
+research_areas = (0..99).to_a.map do |n|
   ResearchArea.create!(area: list_research_areas[n]) 
 end
 
 #Bulk insert professor data
-values = (0..1000).to_a.map {|u| 
+values = (0..50000).to_a.map {|u| 
   "('"+Faker::Name.name.gsub(/\W/, ' ')+"','"+
   Faker::Internet.email+"',"+
   "'https://graydon.law/wp-content/themes/graydon/images/gravatar_default_550.jpg','"+
@@ -38,7 +133,7 @@ values = (0..1000).to_a.map {|u|
 ActiveRecord::Base.connection.execute("INSERT INTO professors (name, email, picture, phone, school, department, created_at, updated_at) VALUES #{values}")
 
 #Bulk insert student data
-values = (0..1000).to_a.map {|u| 
+values = (0..50000).to_a.map {|u| 
   "('"+Faker::Name.name.gsub(/\W/, ' ')+"','"+
   Faker::Internet.email+"','"+
   list_research_areas[u%10]+"','"+
@@ -48,7 +143,7 @@ values = (0..1000).to_a.map {|u|
 
 ActiveRecord::Base.connection.execute("INSERT INTO students (name, email, researcharea, created_at, updated_at) VALUES #{values}")
 
-values = (0..1000).to_a.map {|u|
+values = (0..50000).to_a.map {|u|
   "("+u.to_s+","+
   rand(research_areas.size).to_s+",'"+
   Time.now.utc.to_s(:db)+"','"+
