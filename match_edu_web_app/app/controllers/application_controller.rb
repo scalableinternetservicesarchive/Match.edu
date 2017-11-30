09ce03260db33da0713ec6901c486a5e31ca9f66
Jiaheng Tang <jiaheng1@vt.edu>
2017-11-29 22:46:35 -0800

    removed forgery protection for load testing

--- match_edu_web_app/app/controllers/application_controller.rb
+++ match_edu_web_app/app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
+ 
   def hello
     render html: "Welcome to Match.edu! \n Look out for something awesome in this space!"
   end
